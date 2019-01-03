fb66c06c06d359e14d3604aff43b5e1224e9bf7f
Shub4am <shub4am@gmail.com>
2019-01-04 03:44:58 +0530

    Updates

--- precache-manifest.1e7834e50013f7f107f2c8130c75e1cf.js
+++ precache-manifest.1e7834e50013f7f107f2c8130c75e1cf.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "575250b0dce792460e20",
-    "url": "/portfolio/static/js/main.575250b0.chunk.js"
+    "revision": "8eb8b676956fec0babbb",
+    "url": "/portfolio/static/js/main.8eb8b676.chunk.js"
   },
   {
     "revision": "e850df4d0412d493445c",
     "url": "/portfolio/static/js/1.e850df4d.chunk.js"
   },
   {
-    "revision": "575250b0dce792460e20",
+    "revision": "8eb8b676956fec0babbb",
     "url": "/portfolio/static/css/main.a5be4e6d.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/css/1.1c7acc87.chunk.css"
   },
   {
-    "revision": "e9f3c55296aab77d599a5beac195902c",
+    "revision": "a4de55ea85d9bcb9c5749851905236ae",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6b581542b06d02cf3b11feee007bc91b.js"
+  "/portfolio/precache-manifest.1e7834e50013f7f107f2c8130c75e1cf.js"
 );
 
 workbox.clientsClaim();
