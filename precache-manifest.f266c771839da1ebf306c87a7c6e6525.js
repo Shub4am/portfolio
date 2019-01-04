16cd766b7bf4f051f69b463fc3bf24db60aafd51
Shub4am <shub4am@gmail.com>
2019-01-04 05:53:14 +0530

    Updates

---
+++ precache-manifest.f266c771839da1ebf306c87a7c6e6525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "8eb8b676956fec0babbb",
+    "url": "/portfolio/static/js/main.8eb8b676.chunk.js"
+  },
+  {
+    "revision": "e850df4d0412d493445c",
+    "url": "/portfolio/static/js/1.e850df4d.chunk.js"
+  },
+  {
+    "revision": "8eb8b676956fec0babbb",
+    "url": "/portfolio/static/css/main.a5be4e6d.chunk.css"
+  },
+  {
+    "revision": "e850df4d0412d493445c",
+    "url": "/portfolio/static/css/1.1c7acc87.chunk.css"
+  },
+  {
+    "revision": "bc8329bce71b855679a1365790b206b3",
+    "url": "/portfolio/index.html"
+  }
+];
