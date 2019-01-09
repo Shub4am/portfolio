b10437acfd56f332f9ae60a33f4452843a3bcb9d
Shub4am <shub4am@gmail.com>
2019-01-09 18:22:53 +0530

    update 1.1

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -9,26 +9,29 @@ class Contacts extends Component {
                     <Cell col={6}>
                         <h2 style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>
                         I'd love to hear from you.</h2>
-                        <h3 style={{color:'#54545c'}}>You can always reach out by <b style={{color:'black'}}>e-mail</b>. Or connect with me on <a href="https://www.instagram.com/shub4am/" rel="noopener noreferrer" target="_blank"><b style={{color:'black'}}>Instagram</b></a> or on <a href="https://www.linkedin.com/in/Shubham92101/" rel="noopener noreferrer" target="_blank"><b style={{color:'black'}}>Linkedin</b></a></h3>
+                        <h3 style={{color:'#54545c'}}>You can always reach out by<a href="android-app://com.google.android.gm/" rel="noopener noreferrer" target="_blank"><b style={{color:'black'}}> e-mail</b></a>. Or connect with me on <a href="https://www.instagram.com/shub4am/" rel="noopener noreferrer" target="_blank"><b style={{color:'black'}}>Instagram</b></a> or on <a href="https://www.linkedin.com/in/Shubham92101/" rel="noopener noreferrer" target="_blank"><b style={{color:'black'}}>Linkedin</b></a></h3>
                     </Cell>
                     <Cell col={6}>
                         <h2>Contact Me</h2>
                         <hr/>
-
                         <div className="contact-list">
-									<List>
-										<ListItem>
-											<ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-												<i className="fa fa-phone-square" aria-hidden="true"/>
-											+91 7*********</ListItemContent>
-										</ListItem>
-										<ListItem>
-											<ListItemContent style={{fontSize:'30px', fontFamily: 'Anton'}}>
-												<i className="fa fa-envelope" aria-hidden="true"/>
+								<List>
+									<ListItem>
+										<ListItemContent style={{fontSize: '26px', fontFamily: 'Anton'}}>
+											<i className="fa fa-phone-square" aria-hidden="true"/>(Whatsapp)
+											+91 9905972742</ListItemContent>
+									</ListItem>
+                                    <ListItem>
+										<ListItemContent style={{fontSize: '26px', fontFamily: 'Anton'}}>
+											<i className="fa fa-phone-square" aria-hidden="true"/>
+											+917*********</ListItemContent>
+									</ListItem>
+									<ListItem>
+										<ListItemContent style={{fontSize:'22px', fontFamily: 'Anton'}}>
+											<i className="fa fa-envelope" aria-hidden="true"/>
 											shubham92101@gmail.com</ListItemContent>
-										</ListItem>
-										
-										</List>
+									</ListItem>
+								</List>
                         </div>
                     </Cell>
                 </Grid>
