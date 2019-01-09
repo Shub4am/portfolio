060f0ef78a6391581c4159dd2f46b42ace5248a3
Shub4am <shub4am@gmail.com>
2019-01-09 22:51:13 +0530

    Update 1.2

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,7 +11,7 @@ class Projects extends Component {
         if(this.state.activeTab === 0){
             return(
                 <div className="projects-grid">
-                {/*Project 1 */},    
+                {/*PROJECT TAB 1 REACT JS*/},    
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
                     <CardTitle style={{color: '#ffffff', height: '250px', background:'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center / cover'}}>React Project
                     </CardTitle>
@@ -19,14 +19,14 @@ class Projects extends Component {
                         Portfolio Website built using ReactJS
                     </CardText>
                     <CardActions border>
-                    <a href="https://github.com/Shub4am/Portfolio-Website" rel="noopener noreferrer" target="_blank">Github</a>
+                    <h4><a href="https://github.com/Shub4am/Portfolio" rel="noopener noreferrer" target="_blank"><b style={{color:'black'}}>Github</b></a></h4>
                     </CardActions>
                     <CardMenu style={{color: '#fff'}}>
                         <IconButton name="share"/>
                     </CardMenu>
                 </Card>
 
-                {/*Project 2 */},    
+                {/*react js */},    
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
                     <CardTitle style={{color: '#ffffff', height: '250px', background:'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center / cover'}}>React Project
                     </CardTitle>
@@ -34,7 +34,7 @@ class Projects extends Component {
                         StopWatch built using React.js.
                     </CardText>
                     <CardActions border>
-                        <Button colored>Github</Button>
+                    <h4><a href="https://github.com/Shub4am/ReactNative-StopWatch" rel="noopener noreferrer" target="_blank"><b style={{color:'black'}}>Github</b></a></h4>
                     </CardActions>
                     <CardMenu style={{color: '#fff'}}>
                         <IconButton name="share"/>
@@ -45,65 +45,64 @@ class Projects extends Component {
         }else if(this.state.activeTab === 1){
             return(
                 <div className="projects-grid">
-                {/*Project 1*/},    
+                {/*Project TAB 2 ANDROID PROJECTS*/}},    
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color: '#000', height: '175px', background:'url(https://banner2.kisspng.com/20180326/pjw/kisspng-android-studio-integrated-development-environment-studio-5ab9a802275d15.7223949015221166101612.jpg) center / cover'}}>Popular Movies App
+                    <CardTitle style={{color: '#000', height: '250px', background:'url(https://banner2.kisspng.com/20180326/pjw/kisspng-android-studio-integrated-development-environment-studio-5ab9a802275d15.7223949015221166101612.jpg) center / cover'}}>Popular Movies App
                     </CardTitle>
                     <CardText>
                     Android app that uses The MovieDB API to show the most popular, highest rated and user's favorites movies details.
                     </CardText>
                     <CardActions border>
-                        <Button colored>Github</Button>
+                    <h4><a href="https://github.com/Shub4am/Popular-Movies" rel="noopener noreferrer" target="_blank"><b style={{color:'black'}}>Github</b></a></h4>
                     </CardActions>
-                    <CardMenu style={{color: '#fff'}}>
+                    <CardMenu style={{color: '#000'}}>
                         <IconButton name="share"/>
                     </CardMenu>
                 </Card>
                 
-                {/*Project 2*/},    
+                {/*ANDROID PROJ*/},    
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color: '#000', height: '175px', background:'url(https://banner2.kisspng.com/20180326/pjw/kisspng-android-studio-integrated-development-environment-studio-5ab9a802275d15.7223949015221166101612.jpg) center / cover'}}>Blocks
+                    <CardTitle style={{color: '#000', height: '250px', background:'url(https://banner2.kisspng.com/20180326/pjw/kisspng-android-studio-integrated-development-environment-studio-5ab9a802275d15.7223949015221166101612.jpg) center / cover'}}>Blocks
                     </CardTitle>
                     <CardText>
                     Android game that stacks 3-d cubes.
                     </CardText>
                     <CardActions border>
-                        <Button colored>Github</Button>
+                    <h4><a href="https://github.com/Shub4am/Blocks" rel="noopener noreferrer" target="_blank"><b style={{color:'black'}}>Github</b></a></h4>
                     </CardActions>
-                    <CardMenu style={{color: '#fff'}}>
+                    <CardMenu style={{color: '#000'}}>
                         <IconButton name="share"/>
                     </CardMenu>
                 </Card>
-                
                 </div>
             )
         }else if(this.state.activeTab === 2){
             return(
                 <div className="projects-grid">
-                {/*Project 1 */},    
+                {/*Project TAB 3 JAVASCRIPT PROJECTS*/},    
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color: '#ff0000', height: '175px', background:'url(https://i.ibb.co/X7sJ7cH/js.jpg) center / cover'}}>React Project#1
+                    <CardTitle style={{color: '#ff0000', height: '250px', background:'url(https://i.ibb.co/X7sJ7cH/js.jpg) center / cover'}}>JS Project#1
                     </CardTitle>
                     <CardText>
-                        Portfolio Website built using javascript framework React.js.
+                        Portfolio Website built using JavaScript and also React.js.
                     </CardText>
                     <CardActions border>
-                    <Button onClick="location.href='https://github.com/Shub4am/Portfolio-Website';">Github</Button>
+                    <h4><a href="https://github.com/Shub4am/Portfolio//tree/gh-pages" rel="noopener noreferrer" target="_blank"><b style={{color:'black'}}>Github</b></a></h4>
                     </CardActions>
                     <CardMenu style={{color: '#fff'}}>
                         <IconButton name="share"/>
                     </CardMenu>
                 </Card>
 
-                {/*Project 2 */},    
+                {/*JAVASCRIPT PROJECTS*/},    
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color: '#ff0000', height: '175px', background:'url(https://i.ibb.co/X7sJ7cH/js.jpg) center / cover'}}>JavaScript Projects
+                    <CardTitle style={{color: '#ff0000', height: '250px', background:'url(https://i.ibb.co/X7sJ7cH/js.jpg) center / cover'}}>JS Projects
                     </CardTitle>
                     <CardText>
                         Other short projects done in JS
                     </CardText>
                     <CardActions border>
-                        <Button colored>Github</Button>
+                    <h4><a href="https://github.com/Shub4am/JSProject" rel="noopener noreferrer" target="_blank"><b style={{color:'black'}}>Github</b></a></h4>
                     </CardActions>
                     <CardMenu style={{color: '#fff'}}>
                         <IconButton name="share"/>
@@ -114,30 +113,29 @@ class Projects extends Component {
         }else if(this.state.activeTab ===3){
             return(
                 <div className="projects-grid">
-                {/*Project 1*/},    
+                {/*Project TAB 4 JAVA PROJECTS*/},    
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color: '#fc5d5d', height: '175px', background:'url(https://i.ibb.co/jbhCx1x/java-logo.png) center / cover'}}>Popular Movies App
+                    <CardTitle style={{color: '#000000', height: '250px', background:'url(https://seofiles.s3.amazonaws.com/seo/media/uploads/2017/01/24/unit-41-java.png) center / cover'}}>Popular Movies
                     </CardTitle>
                     <CardText>
                     Android app that uses The MovieDB API to show the most popular, highest rated and user's favorites movies details.
                     </CardText>
                     <CardActions border>
-                        <Button colored>Github</Button>
+                    <h4><a href="https://github.com/Shub4am/Popular-Movies" rel="noopener noreferrer" target="_blank"><b style={{color:'black'}}>Github</b></a></h4>
                     </CardActions>
                     <CardMenu style={{color: '#000'}}>
                         <IconButton name="share"/>
                     </CardMenu>
                 </Card>
-                
-                {/*Project 2*/},    
+                {/*Project TAB 4 JAVA PROJECTS*/},    
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color: '#fc5d5d',fontWeight: 'bolder' , height: '175px', background:'url(https://i.ibb.co/jbhCx1x/java-logo.png) center / cover'}}>Blocks
+                    <CardTitle style={{color: '#000000',fontWeight: 'bolder' , height: '250px', background:'url(https://seofiles.s3.amazonaws.com/seo/media/uploads/2017/01/24/unit-41-java.png) center / cover'}}>Java Projects
                     </CardTitle>
                     <CardText>
-                    Android game that stacks 3-d cubes.
+                    Others Java Projects.
                     </CardText>
                     <CardActions border>
-                        <Button colored>Github</Button>
+                    <h4><a href="https://github.com/Shub4am/JavaProject" rel="noopener noreferrer" target="_blank"><b style={{color:'black'}}>Github</b></a></h4>
                     </CardActions>
                     <CardMenu style={{color: '#000'}}>
                         <IconButton name="share"/>
